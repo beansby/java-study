f3e814a743432e2d08af5390976f37ad498c7274
Yebin Ha <beanswebdev@gmail.com>
2023-01-27 21:33:25 +0900

    이것이 자바다 7장- 확인문제 주석처리

--- ClassInheritance_Abstract/Question12/Example.java
+++ ClassInheritance_Abstract/Question12/Example.java
@@ -6,9 +6,9 @@ package ClassInheritance_Abstract.Question12;
 public class Example {
     public static void action(A a){
         a.method1();
-        if (a instanceof C c) {
-            c.method2();
-        }
+//        if (a instanceof C c) {
+//            c.method2();
+//        }
     }
 
     public static void main(String[] args) {
